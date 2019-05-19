7eea3f41de75962a322bd70938b78b198e9ab45c
Carles Capellas <capellas.carles@gmail.com>
2019-05-19 12:02:41 +0200

    Use environment variables meaningfully

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const mainApp = express();
 // TODO Add cucumber plugin for Cypress
 
 const defaultConfig = {
-    CONFIG_PARAMETER: 'Value placeholder'
+    PORT: 3000
 };
 
 const importResult = dotenv.config();
@@ -25,9 +25,10 @@ if (importResult.error) {
     console.log(`No environment configuration found at ${__dirname}`);
 }
 
-// Getting the values from process.env instead of importResult.parsed so that they can easily be injected in Docker
+// Getting the values from process.env instead of importResult.parsed so that
+// they can directly be injected in Docker through process.env
 const environmentConfig = {
-    CONFIG_PARAMETER: process.env.CONFIG_PARAMETER !== undefined ? process.env.CONFIG_PARAMETER : defaultConfig.CONFIG_PARAMETER
+    PORT: process.env.PORT !== undefined ? process.env.PORT : defaultConfig.PORT
 };
 
 const appsPath = path.join(__dirname, 'apps');
@@ -38,8 +39,8 @@ mainApp.use(getRequestResolver(apps));
 mainApp.use(getRenderIsolator(appsPath));
 
 exposeHostedApps(mainApp, apps)
-    .then(_ => launchServer(mainApp, { port: 3000 }))
+    .then(_ => launchServer(mainApp, { port: environmentConfig.PORT }))
     .then(_ => {
-        console.log(`Server up & running in port 3000 (with CONFIG_PARAMETER="${environmentConfig.CONFIG_PARAMETER}")`);
+        console.log(`Server up & running in port ${environmentConfig.PORT}`);
     })
     .catch(error => console.log(error));
